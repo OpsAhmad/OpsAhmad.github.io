934b3e6c5949ec2bee48e50423d9507ea0842825
OpsAhmad <alendra056@gmail.com>
2020-09-16 10:23:01 +0700

    fixing bugs
    Fixing javascript variable problem

--- rgb/assets/js/script.js
+++ rgb/assets/js/script.js
@@ -2,8 +2,8 @@ var picker = document.querySelector(".slider");
 
 picker.addEventListener("change", function () {
   var red = document.querySelector(".red").value;
-  var green = document.querySelector(".blue").value;
-  var blue = document.querySelector(".green").value;
+  var green = document.querySelector(".green").value;
+  var blue = document.querySelector(".blue").value;
 
   var rgb = "rgb(" + red + "," + green + "," + blue + ")";
 
